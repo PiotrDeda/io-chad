41c5eda8012525453c89ed955b37d743276ecb94
vis4rd <alagaesia.vrael@gmail.com>
2022-04-23 13:27:22 +0200

    add LoginButton component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import LoginButton from './components/buttons/LoginButton.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
+    <LoginButton link="https://google.com" /> <!-- debug -->
     <TheWelcome />
   </main>
 </template>
@@ -44,11 +46,11 @@ a,
   transition: 0.4s;
 }
 
-@media (hover: hover) {
+/* @media (hover: hover) {
   a:hover {
     background-color: hsla(160, 100%, 37%, 0.2);
   }
-}
+} */
 
 @media (min-width: 1024px) {
   body {
---
+++ src/components/buttons/LoginButton.vue
@@ -0,0 +1,43 @@
+<script setup>
+    defineProps(
+    {
+        link:
+        {
+            type: String,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <a v-bind:href="link"><button type="button">Login</button></a>
+</template>
+
+<style scoped>
+button
+{
+    text-align: center;
+    color: var(--chad-c-green);
+
+    margin: 0;
+    padding: 5px 15px;
+    background-color: transparent;
+
+    border-style: solid;
+    border-radius: 6px;
+    border-width: 1px;
+    border-color: var(--chad-c-border);
+
+    transition: 0.4s;
+}
+
+@media(hover: hover)
+{
+    button:hover
+    {
+        border-color: var(--chad-c-border-hover);
+        color: var(--chad-c-green-hover);
+        background-color: var(--chad-c-element-background-hover);
+    }
+}
+</style>
