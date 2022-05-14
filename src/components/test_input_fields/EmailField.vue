5934d2a4d33c1bc42070cb9ecc583f1854cad092
vis4rd <alagaesia.vrael@gmail.com>
2022-05-14 19:56:50 +0200

    add EmailField component

---
+++ src/components/test_input_fields/EmailField.vue
@@ -0,0 +1,58 @@
+<script setup>
+    const props = defineProps(
+    {
+        label: { type: String, required: true },
+        minLength: {type: Number, default: 5 },
+        maxLength: { type: Number, default: 30 },
+        autofocus: { type: Boolean, default: false},
+        disabled: { type: Boolean, default: false }
+    });
+</script>
+
+<template>
+    <input
+        type="email"
+        :autofocus="props.autofocus"
+        form=""
+        name="email"
+        :minlength="props.minLength"
+        v-bind:maxlength="props.maxLength"
+        :size="props.maxLength"
+        pattern="/^[a-z0-9_\-\.]{2,}@[a-z0-9_\-\.]{2,}\.[a-z]{2,}$/i"
+        placeholder="email address"
+        :disabled="props.disabled"
+        required
+    />
+</template>
+
+<style scoped>
+input[type="email"]::placeholder
+{
+    color: var(--chad-c-gray);
+}
+
+input[type="email"]
+{
+    color: var(--chad-c-green);
+    font-size: inherit;
+
+    margin: 4px;
+    padding: 7.5px 15px;
+    background-color: transparent;
+
+    border-style: solid;
+    border-radius: 6px;
+    border-width: 1px;
+    border-color: var(--chad-c-border);
+    /* outline-color: var(--chad-c-border-hover); */
+
+    transition: 0.4s;
+}
+
+input[type="email"]:focus, input[type="email"]:hover
+{
+    outline: none;
+    border-color: var(--chad-c-border-hover);
+}
+
+</style>
