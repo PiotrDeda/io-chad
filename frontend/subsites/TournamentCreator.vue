49c25712db4b1cb6259725e2b9341c86b2eae503
vis4rd <alagaesia.vrael@gmail.com>
2022-05-23 20:59:47 +0200

    Change setup of TournamentCreator sections

--- frontend/subsites/TournamentCreator.vue
+++ frontend/subsites/TournamentCreator.vue
@@ -1,16 +1,29 @@
 <script setup>
 import TopPanel from '../components/panels/TopPanel.vue';
+import IntegerField from '../components/test_input_fields/IntegerField.vue';
 </script>
 
 <template>
     <TopPanel />
     <header>
         <h1>Kreator Turnieju</h1>
-        <h1>Kreator Turnieju</h1>
     </header>
 
     <main>
-        
+        <article>
+
+        </article>
+        <aside>
+            <IntegerField label="Liczba uczestników" min="0" max="12" value="4" />
+            <IntegerField label="Mecze bezpośrednie" min="0" max="4" value="2" />
+            <IntegerField label="Liczba grup" min="0" max="20" value="2" />
+            <IntegerField label="Punkty do bezpośredniego awansu" min="0" max="99" value="2" />
+            <IntegerField label="Punkty do baraży" min="0" max="99" value="2" />
+            <IntegerField label="Punkty do awansu warunkowego*" min="0" max="99" value="2" />
+            <IntegerField label="Punkty za wygraną" min="0" max="99" value="3" />
+            <IntegerField label="Punkty za remis" min="-99" max="99" value="1" />
+            <IntegerField label="Punkty za przegraną" min="-99" max="99" value="0" />
+        </aside>
     </main>
 </template>
 
@@ -20,42 +33,67 @@ import TopPanel from '../components/panels/TopPanel.vue';
 body
 {
     place-items: center;
-    /* border: solid 2px white; */
 }
 
 #app
 {
+    display: flex;
+    flex-direction: column;
+    
+    justify-content: flex-start;
+    align-items: center;
+
     position: absolute;
-    top: 50px;
 
     margin: 0;
     padding: 0;
+    padding-top: 50px;
 
     width: 100%;
-    min-height: calc(var(inherit) - 50px);
+    min-height: inherit;
 
     font-weight: normal;
-    border: solid 2px red;
+    /* border: solid 2px red; */
 }
 
 header
 {
     text-align: center;
-    border: solid 1px white;
 }
 
 h1
 {
     justify-content: center;
-    border: solid 1px white;
 }
 
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
+main
+{
+    display: flex;
+
+    min-height: fit-content;
+    width: inherit;
+
+    /* border: solid 1px red; */
 }
 
-a,
+article
+{
+    width: 70%;
+}
+
+aside
+{
+    /* border: solid 1px white; */
+    padding: 15px;
+    margin: 15px;
+    width: min(30%, fit-content);
+
+    border-style: solid;
+    border-radius: 6px;
+    border-width: 1px;
+    border-color: var(--chad-c-border);
+}
+
 .green {
     text-decoration: none;
     color: hsla(160, 100%, 37%, 1);
