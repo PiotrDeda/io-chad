6ade4cda2fb3007f36f82bdd8e4413923f398b90
PiotrDeda <piotrdeda.pl@gmail.com>
2022-06-10 23:58:16 +0200

    Axios + CORS

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const accounts = require('./accounts');
 const competitions = require('./competitions');
 const matches = require('./matches');
@@ -7,11 +8,13 @@ const port = 8000;
 
 const mongoDB = 'mongodb+srv://admin:admin@io-chad.elbrc.mongodb.net/IO-CHAD?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+	origin: 'http://localhost:3000'
+}));
 app.listen(port, () => console.log("Node Express server listening at port " + port))
 
 app.use('/accounts', accounts)
