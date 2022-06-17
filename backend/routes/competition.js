11cea0eeb2d85f620ae1d460ea7e28b1e7c5a674
PiotrDeda <piotrdeda.pl@gmail.com>
2022-06-18 00:09:32 +0200

    Krótsze routy

--- backend/routes/competition.js
+++ backend/routes/competition.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const auth = require('../config/auth');
 const competitionController = require('../controllers/competition');
 
-router.post('/competitions', auth, competitionController.post);
-router.get('/competitions', auth, competitionController.getAll);
-router.get('/competitions/:id', auth, competitionController.get);
-router.delete('/competitions', auth, competitionController.delete);
-router.put('/competitions', auth, competitionController.put);
+router.post('/', auth, competitionController.post);
+router.get('/', auth, competitionController.getAll);
+router.get('/:id', auth, competitionController.get);
+router.delete('/', auth, competitionController.delete);
+router.put('/', auth, competitionController.put);
 
 module.exports = router;
