7942887842a16177b5f93e22fad0c1b309a4a526
PiotrDeda <piotrdeda.pl@gmail.com>
2022-06-18 00:14:59 +0200

    Brakujące :id

--- backend/routes/competition.js
+++ backend/routes/competition.js
@@ -6,7 +6,7 @@ const competitionController = require('../controllers/competition');
 router.post('/', auth, competitionController.post);
 router.get('/', auth, competitionController.getAll);
 router.get('/:id', auth, competitionController.get);
-router.delete('/', auth, competitionController.delete);
-router.put('/', auth, competitionController.put);
+router.delete('/:id', auth, competitionController.delete);
+router.put('/:id', auth, competitionController.put);
 
 module.exports = router;
