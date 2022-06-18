22c8fe30d5dc32bf800cd04016686386f6c6815d
PiotrDeda <piotrdeda.pl@gmail.com>
2022-06-19 01:57:14 +0200

    Integracja z backendem

--- frontend/views/BracketDesigner.vue
+++ frontend/views/BracketDesigner.vue
@@ -3,96 +3,82 @@ import TopPanel from '../components/panels/TopPanel.vue';
 import Draggable from '../components/draggable/Draggable.vue';
 import ElementsList from '../components/lists/ElementsList.vue';
 import DropZone from '../components/draggable/DropZone.vue';
+import {onMounted, ref} from 'vue';
+import axios from "axios";
 
-import {ref} from 'vue';
+// TODO: debug variable, remove when not needed
+const tournamentId = '62acfdc25060280a13aa25b3';
 
-// zaladowanie info o turnieju
-const props = defineProps({
-    amount: {type: Number, required: false, default: 16},
-    tournament: {type: String, required: false, default: "Turniej"},
-    pairs: {type: Array, required: false, default: []}
-});
+const tournament = ref({});
+const amount = ref(0);
+const pairs = ref([]);
 
 // zaladowanie druzyn
-const comps = ref([
-    {name: 'jeden', list: 1},
-    {name: 'dwa', list: 1},
-    {name: 'trzy', list: 1},
-    {name: 'cztery', list: 1},
-    {name: 'piec', list: 1},
-    {name: 'szesc', list: 1},
-    {name: 'siedem', list: 1},
-    {name: 'osiem', list: 1},
-    {name: 'dziewiec', list: 1},
-    {name: 'dziesiec', list: 1},
-    {name: 'jedenascie', list: 1},
-    {name: 'dwanascie', list: 1},
-    {name: 'trzynascie', list: 1},
-    {name: 'czternascie', list: 1},
-    {name: 'pietnascie', list: 1},
-    {name: 'szesnascie', list: 1},
-])
+const comps = ref([])
 
-const loadPairs = () => {
-    for (let i = 1; i < 1 + props.amount / 2; i++) {
-        console.log(props.amount)
-        props.pairs.push({pair_id: i, home_id: i * 2, away_id: i * 2 + 1})
+function loadPairs() {
+    for (let i = 1; i < 1 + amount.value / 2; i++) {
+        pairs.value.push({pair_id: i, home_id: i * 2, away_id: i * 2 + 1});
     }
-    console.log(props.pairs)
 }
 
-const onDrop = (event, list, limit) => {
+function onDrop(event, list, limit) {
     const new_name = event.dataTransfer.getData('item');
-    const new_item = comps.value.find((it) => it.name === new_name)
-    if (getTeams(list).length >= limit) {
-        return
-    }
-    new_item.list = list
-    console.log(list)
+    const new_item = comps.value.find((it) => it.name === new_name);
+    if (getTeams(list).length >= limit)
+        return;
+    new_item.list = list;
 }
 
-const getTeams = (list) => {
-    return comps.value.filter((it) => it.list === list)
+function getTeams(list) {
+    return comps.value.filter((it) => it.list === list);
 }
 
 function resetLayout() {
-    comps.value.forEach((it) => {
-        it.list = 1;
-    })
+    comps.value.forEach((it) => it.list = 1);
 }
 
-loadPairs()
-
+onMounted(async () => {
+    await axios.get('http://localhost:8000/competitions/' + tournamentId, {headers: {"Authorization": 'Bearer ' + localStorage.getItem("jwt")}})
+        .then(response => (tournament.value = response.data.competition))
+        .catch(error => {
+            console.log(error);
+            alert(error.response.data.message);
+        })
+    amount.value = tournament.value.participants.length;
+    comps.value = tournament.value.participants.map((it) => ({name: it.name, list: 1}));
+    loadPairs();
+})
 </script>
 
 <template>
     <TopPanel/>
     <header>
-        <h1> {{ tournament }} </h1>
+        <h1> {{ tournament.name }} </h1>
         <h3> Kreator drabinki turniejowej </h3>
     </header>
 
     <main id="main">
         <DropZone id="team_selector"
+                  :items="getTeams(1)"
                   placeholder=""
-                  @drop="onDrop($event, -1)"
-                  :items="getTeams(-1)"
+                  @drop="onDrop($event, 1)"
         >
             <div class="play_off_title"> Uczestnicy</div>
-            <ElementsList height="498px" width="100%" id="team_listing">
+            <ElementsList id="team_listing" height="498px" width="100%">
                 <Draggable v-for="it in getTeams(1)" :item="it.name"></Draggable>
             </ElementsList>
         </DropZone>
 
         <div id="bracket_layout">
-            <ElementsList width="1000px" height="600px">
+            <ElementsList height="600px" width="1000px">
                 <div id="center">
                     <div v-for="pair in pairs" class="play_off">
                         <div class="play_off_title"> Para {{ pair.pair_id }}</div>
                         <div class="play_off_teams">
                             <DropZone
+                                :items="getTeams(pair.home_id)"
                                 @drop="onDrop($event, pair.home_id, 1)"
-                                :items="getTeams( pair.home_id )"
                             >
                                 <Draggable v-for="it in getTeams(pair.home_id)" :item="it.name"></Draggable>
                             </DropZone>
@@ -102,8 +88,8 @@ loadPairs()
                             </div>
 
                             <DropZone
+                                :items="getTeams(pair.away_id)"
                                 @drop="onDrop($event, pair.away_id, 1)"
-                                :items="getTeams( pair.away_id )"
                             >
                                 <Draggable v-for="it in getTeams(pair.away_id)" :item="it.name"></Draggable>
                             </DropZone>
